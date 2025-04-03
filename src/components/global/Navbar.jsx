0201daea96003a3fab39f84b2f522dbb2d1ef6c9
Yogawan <yogawanadityapratama@gmail.com>
2025-04-03 17:56:17 +0700

    Mengubah design Navbar

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -3,8 +3,8 @@ import ProtectedImage from "../utilities/ProtectedImage";
 
 const Navbar = () => {
     return (
-        <div className="fixed top-5 left-0 right-0 flex items-center justify-center">
-            <nav className="flex w-fit bg-white rounded-full">
+        <div className="z-50 fixed top-5 left-0 right-0 flex items-center justify-center">
+            <nav className="flex w-fit bg-white/50 backdrop-blur rounded-full">
                 <ul className="flex justify-center items-center text-black/75">
                     <li className="w-16">
                         <ProtectedImage
@@ -13,14 +13,17 @@ const Navbar = () => {
                             className="w-full h-16 rounded-full"
                         />
                     </li>
-                    <li className="ml-5 mr-5">
+                    <li className="ml-3 mr-3">
                         <Link className="text-xs" href="/">Beranda</Link>
                     </li>
-                    <li className="ml-5 mr-5">
+                    <li className="ml-3 mr-3">
                         <Link className="text-xs" href="/proyek">Proyek</Link>
                     </li>
-                    <li className="ml-5 mr-5">
+                    <li className="ml-3 mr-3">
                         <Link className="text-xs" href="/berita">Berita</Link>
+                    </li>
+                    <li className="px-3 py-2 mr-3 bg-white rounded-xl">
+                        <Link className="text-xs" href="/berita">Login</Link>
                     </li>
                 </ul>
             </nav>
